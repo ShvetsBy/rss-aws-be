bc91da451cfeb51ff8c9e85f868a0c839b200a18
Ivan Shvets <shvets.ivan@gmail.com>
2022-10-02 21:26:44 +0300

    fix: getProductsList

--- product-service/handlers/getProductsList.js
+++ product-service/handlers/getProductsList.js
@@ -1,13 +1,6 @@
-// import products from "../DB/products";
+import products from "../DB/products";
 var AWS = require("aws-sdk");
 
-// const credentials = new AWS.SharedIniFileCredentials();
-// AWS.config.credentials = credentials;
-// AWS.config.update({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   region: "eu-west-1",
-// });
 var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
 var params = {
@@ -16,27 +9,25 @@ var params = {
 
 async function getItems() {
   try {
-    const data = await ddb
-      .scan(params)
-      .promise()
-      .then((res) => res.Items);
+    const data = await ddb.scan(params).promise();
+    // .then((res) => res.Items);
     return data;
   } catch (err) {
-    return err;
+    return JSON.stringify(err.message);
   }
 }
 
-export const getProductsList = async (event, context, callback) => {
+export const getProductsList = async (event) => {
   try {
     const data = await getItems();
-
     return {
       statusCode: 200,
       headers: {
+        "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "*",
       },
-      data: JSON.stringify(data),
+      body: JSON.stringify(data),
     };
   } catch (err) {
     return { err: "ept" };
