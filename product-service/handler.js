460d08f5b0a164772b225b99b1052137a9abd6fa
Ivan Shvets <shvets.ivan@gmail.com>
2022-10-02 23:38:34 +0300

    add get stock api

--- product-service/handler.js
+++ product-service/handler.js
@@ -2,3 +2,4 @@
 
 export { getProductsList } from "./handlers/getProductsList";
 export { getProductById } from "./handlers/getProductById";
+export { getStock } from "./handlers/getStock";
---
+++ product-service/handlers/getStock.js
@@ -0,0 +1,33 @@
+var AWS = require("aws-sdk");
+
+var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+var params = {
+  TableName: process.env.TABLE_NAME,
+};
+
+async function getItems() {
+  try {
+    const data = await ddb.scan(params).promise();
+    return data;
+  } catch (err) {
+    return JSON.stringify(err.message);
+  }
+}
+
+export const getStock = async (event) => {
+  try {
+    const data = await getItems();
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+      },
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    return { err: "ept" };
+  }
+};
