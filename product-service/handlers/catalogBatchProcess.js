8b2fe3d8a15ec687716a0132490a529b1334cee4
Ivan Shvets <shvets.ivan@gmail.com>
2022-10-16 23:52:03 +0300

    fix: task 6.3

--- product-service/handlers/catalogBatchProcess.js
+++ product-service/handlers/catalogBatchProcess.js
@@ -2,24 +2,24 @@ import AWS from "aws-sdk";
 import { putProduct } from "./putProduct";
 
 export const catalogBatchProcess = async (event) => {
-  const sns = new AWS.SNS();
   const products = event.Records.map(({ body }) => body);
 
   try {
+    const sns = new AWS.SNS();
     for (const product of products) {
-      console.log(product);
-
       await putProduct(product);
-      sns.publish(
-        {
-          Subject: "new product added in AWS Course",
-          message: JSON.stringify(product),
-          TopicArn: "arn:aws:sns:eu-west-1:203064053127:createProductTopic",
-        },
-        () => {
-          console.log("email sent: " + JSON.stringify(product));
-        }
-      );
+      await sns
+        .publish(
+          {
+            Subject: "new product added in AWS Course",
+            Message: JSON.stringify(product),
+            TopicArn: process.env.SNS_ARN,
+          },
+          () => {
+            console.log("email sent: " + JSON.stringify(product));
+          }
+        )
+        .promise();
     }
 
     console.log({
