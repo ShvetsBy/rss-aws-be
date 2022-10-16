a93a6f1d3b97a072ce2debc23f669b1761790c73
Ivan Shvets <shvets.ivan@gmail.com>
2022-10-16 22:00:15 +0300

    add Task 6.1

--- product-service/handlers/catalogBatchProcess.js
+++ product-service/handlers/catalogBatchProcess.js
@@ -1,4 +1,25 @@
-export const catalogBatchProcess = (event) => {
+import { putProduct } from "./putProduct";
+
+export const catalogBatchProcess = async (event) => {
   const products = event.Records.map(({ body }) => body);
-  console.log(products);
+
+  try {
+    for (const product of products) {
+      console.log(product);
+
+      await putProduct(product);
+    }
+
+    console.log({
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+      },
+      body: JSON.stringify({ msg: "product created!" }),
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
